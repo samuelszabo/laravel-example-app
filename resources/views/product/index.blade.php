b4e5735990ead86db2a730d54e6cc8d896e0135e
Samuel Szabo <samuelszabo7+git@gmail.com>
2022-01-07 13:16:35 +0100

    layout bootstrap

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,19 +1,19 @@
-<div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-        <a href="/" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Some</a>
-        {{--            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>--}}
+@extends('layout')
 
-        {{--            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>--}}
-
-
-        {{--                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>--}}
-
-    </div>
-
-</div>
-@foreach($products as $product)
-    <h1>{{$product['name']}}</h1>
-    <p>{{$product['price']}}</p>
-@endforeach
+@section('title')
+    <title>Products</title>
+@endsection
+@section('content')
+    @foreach($products as $product)
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{$product['name']}}</h5>
+                <p class="card-text">{{$product['price']}}</p>
+                <a href="/product/{{$product['id']}}" class="btn btn-primary">Go {{$product['id']}}</a>
+            </div>
+        </div>
+        <h1></h1>
+        <p></p>
+    @endforeach
+@endsection
 
